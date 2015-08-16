1b424fbfa3b33e98c54827e3d542d45ef5ff669f
James Slack-Smith <jpslacksmith@hotmail.com.au>
2015-08-16 21:42:08 +1000

    Sketch to test compiling with libraries
    Sketch counts up on the 7 seg display. Uses the "Bike" library.

---
+++ Sketches/UsingLibrariesTest/UsingLibrariesTest.ino
@@ -0,0 +1,23 @@
+#include <LEDring_functions.h>
+#include <LEDring_patterns.h>
+#include <pin_definitions.h>
+#include <sevenSeg_functions.h>
+
+
+void setup() {
+  pinMode(BLpin, OUTPUT);
+  pinMode(ABCDpin0, OUTPUT);
+  pinMode(ABCDpin1, OUTPUT);
+  pinMode(ABCDpin2, OUTPUT);
+  pinMode(ABCDpin3, OUTPUT);
+  pinMode(LDpin0, OUTPUT);
+  pinMode(LDpin1, OUTPUT);
+  pinMode(LDpin2, OUTPUT);
+}
+
+void loop() {
+  for( int i =0; i < 10; i++ ) {
+    sevenSeg_set( i );
+    delay(200);
+  }
+}

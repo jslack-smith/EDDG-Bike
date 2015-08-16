56749bf0c459215822b104967d3f994d0cc98098
James Slack-Smith <jpslacksmith@hotmail.com.au>
2015-08-16 21:41:01 +1000

    Moved folders
    Sketches now in a "Sketches" folder and header and .cpp files now in
    "libraries"

---
+++ libraries/Bike/LEDring_functions.h
@@ -0,0 +1,21 @@
+#ifndef LEDRING_FUNCTIONS_H
+#define LEDRING_FUNCTIONS_H
+
+// LEDring_functions.h
+//   Contains functions to interface with the LED ring
+
+/* Circuit Specs:
+    - 2 74HC595 IC's
+    - 2 High Current Darlington Array IC's
+    - 16 segments
+
+*/
+
+#include "pin_definitions.h"
+
+// takes a array of 16-bit shorts (pattern[]) and displays it on the LEDring. Each short in pattern is
+//  displayed for "period" ms. Uses delays.
+void LEDring_patternDelay( short pattern[], int period );
+void LEDring_set( int score );
+
+#endif

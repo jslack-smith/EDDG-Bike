8948c8b104291e98988df5df2d6738863b23db48
James Slack-Smith <jpslacksmith@hotmail.com.au>
2015-08-21 16:56:07 +1000

    Sketch to test Potentiometer + LEDring
    Displays value measured from potentiometer on LED ring. Simulates rider
    score.

---
+++ Sketches/PotAndLEDringTest/PotAndLEDringTest.ino
@@ -0,0 +1,25 @@
+// UsingLibrariesTest.ino
+//    Testing custom built library. Counts up from 0 to 9 on the 7 seg display.
+
+#include <LEDring_functions.h>
+#include <LEDring_patterns.h>
+#include <pin_definitions.h>
+#include <sevenSeg_functions.h>
+
+int sensorValue = 0;
+//int sensorPin = A0;
+
+void setup() {
+  pinMode( ring_dataPin, OUTPUT );
+  pinMode( ring_clkPin, OUTPUT );
+  pinMode( ring_latchPin, OUTPUT );  
+}
+
+void loop() {
+  
+  sensorValue = analogRead( sensorIn );
+    
+    LEDring_set(sensorValue);
+    delay(20);
+    
+}

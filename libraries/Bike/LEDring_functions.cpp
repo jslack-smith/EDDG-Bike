2d37fc1288ea0b1040d9b66656ae2e00830aae58
jalinbank <jalinbank@users.noreply.github.com>
2015-08-30 10:39:13 +1000

    Update LEDring_functions.cpp
    Clean up, added initialisation function

--- libraries/Bike/LEDring_functions.cpp
+++ libraries/Bike/LEDring_functions.cpp
@@ -1,45 +1,95 @@
-// LEDring_functions.cpp
-//  temporary container for LED ring function definitions
+/*  LEDring_functions.cpp
+ *   
+ *   This file contains all function definitions for the led ring
+ *   
+ *   Change Log: 
+ *      28th August 2015 (JL)
+ *          - clean up
+ *          - added initialisation function
+ *          - removed unused function prototypes
+ *          - converted binary patterns to hexadecimal notation and expanded to 16 bits
+ *          - added minAnalogInput and maxAnalogInput constants to make expanding easier
+ *          
+ *      27th August 2015 (JL & JSS)
+ *          - fixed bug (inverted output of patterns)
+ *          
+ *      25th August 2015 (JSS)
+ *          - clean up
+ *          - fixed bug (variable and definition conflict of "numberOfSegments")
+ *          
+ *      23rd August 2015 (JSS) 
+ *          - added "numberOfSegments" constant definition
+ *          
+ *      21st August 2015 (JSS)
+ *          - fixed bug (missing semi-colon)
+ *          
+ *      16th August 2015 (JSS)
+ *          - created file with initial definitions
+ */
 
+// include all necessary header files
 #include "Arduino.h"
+#include "pin_definitions.h"
 #include "LEDring_functions.h"
 
-
-void LEDring_set( int score )
-{
-    // map measured value [0, 1204] to number of segments to light up [0, 16]
-    int numOfSegments = 0;
-    numOfSegments = map( score, 1024, 0, 0, 8 );
-
-    // map number of segments to light up to short data
-    byte data = B00000000;
-
-    switch (numOfSegments) {
-    case 0: data = B00000000; break;
-    case 1: data = B00000001; break;
-    case 2: data = B00000011; break;
-    case 3: data = B00000111; break;
-    case 4: data = B00001111; break;
-    case 5: data = B00011111; break;
-    case 6: data = B00111111; break;
-    case 7: data = B01111111; break;
-    case 8: data = B11111111; break;
-    default: data = B00000000; break;
-    }
-
-    // write to LEDring
-    digitalWrite( ring_latchPin, 0 );
-    shiftOut( ring_dataPin, ring_clkPin, LSBFIRST, data );
-    //shiftOut( ring_dataPin, ring_clkPin, LSBFIRST, data>>8 );
-    digitalWrite( ring_latchPin, 1 );
+// LEDring_initialise()
+//     this function intialises the led ring display by
+//     setting all the necessary pins to the correct pin modes
+void LEDring_initialise() {
+  pinMode( ring_dataPin, OUTPUT );
+  pinMode( ring_clkPin, OUTPUT );
+  pinMode( ring_latchPin, OUTPUT ); 
 }
 
+// LEDring_set(int score)
+//     this function lights the corresponding number of segments in the led
+//     ring depending on the score passed as an argument
+void LEDring_set(int score) {
+    
+    // map measured value [0, 1024] to number of segments to light up [0, numOfSegments]
+    int numSegments = 0;
+    numSegments = map(score, minAnalogInput, maxAnalogInput, 0, numOfSegments);
 
-void LEDring_singleSet( short pattern )
-{
-    // write to LEDring
-    digitalWrite( ring_latchPin, 0 );
-    shiftOut( ring_dataPin, ring_clkPin, LSBFIRST, pattern );
-    //shiftOut( ring_dataPin, ring_clkPin, LSBFIRST, data>>8 );
-    digitalWrite( ring_latchPin, 1 );
+    // map number of segments to light up to short data
+    byte data = 0x00;
+
+    // determine the output pattern based on the mapped score
+    switch(numSegments) {
+      case 0:  data = 0x0000; break;
+      case 1:  data = 0x0001; break;
+      case 2:  data = 0x0003; break;
+      case 3:  data = 0x0007; break;
+      case 4:  data = 0x000F; break;
+      case 5:  data = 0x001F; break;
+      case 6:  data = 0x003F; break;
+      case 7:  data = 0x007F; break;
+      case 8:  data = 0x00FF; break;
+      case 9:  data = 0x01FF; break;
+      case 10: data = 0x03FF; break;
+      case 11: data = 0x07FF; break;
+      case 12: data = 0x0FFF; break;
+      case 13: data = 0x1FFF; break;
+      case 14: data = 0x3FFF; break;
+      case 15: data = 0x7FFF; break;
+      case 16: data = 0xFFFF; break;
+      default: data = 0x0000; break;
+    }
+
+    // write the pattern to the led ring
+    LEDring_singleSet(data);
+}
+
+// LEDring_singleSet(short pattern)
+//     this function lights the corresponding segments of the led ring depending 
+//     on the specific pattern passed as an argument
+void LEDring_singleSet( short pattern ) {
+    
+    // XOR the pattern with 1's to invert the pattern (since it is common anode)
+    pattern = 0x00FF^pattern;
+  
+    // write the data to the led ring by shifting the data out
+    digitalWrite(ring_latchPin, 0);
+    shiftOut(ring_dataPin, ring_clkPin, MSBFIRST, pattern);
+    //shiftOut(ring_dataPin, ring_clkPin, MSBFIRST, pattern>>8);
+    digitalWrite(ring_latchPin, 1);
 }

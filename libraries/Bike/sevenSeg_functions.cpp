75b3f01bc4e2fc30d910d19b5d66ed0a15ddfc94
jalinbank <jalinbank@users.noreply.github.com>
2015-08-30 10:45:02 +1000

    Update sevenSeg_functions.cpp
    Clean up, added initialisation function

--- libraries/Bike/sevenSeg_functions.cpp
+++ libraries/Bike/sevenSeg_functions.cpp
@@ -1,87 +1,126 @@
-// sevenSeg_functions.c
-//  temporary holder for sevenSeg function definitions
+/*  sevenSeg_functions.cpp
+ *   
+ *   This file contains all functions for the seven segment display
+ *   
+ *   Change Log: 
+ *      28th August 2015 (JL)
+ *          - clean up
+ *          - added initialisation function
+ *      
+ *      23th August 2015 (JSS) 
+ *          - constants moved from .cpp file to .h file
+ *          
+ *      21st August 2015 (JL & JSS)
+ *          - added blanking functions
+ *          
+ *      16th August 2015 (JSS)
+ *          - created file with initial definitions
+ */
 
+// include all necessary local header files
 #include "Arduino.h"
+#include "pin_definitions.h"
 #include "sevenSeg_functions.h"
 
-
-
-void sevenSeg_blankPin( int pin ) {
-  digitalWrite( pin, !PH ); // enable blanking
+// sevenSeg_initialise()
+//     this function intialises the seven segment display by
+//     setting all the necessary pins to the correct pin modes
+void sevenSeg_initialise() {
+  pinMode(BLpin0, OUTPUT);
+  pinMode(BLpin1, OUTPUT);
+  pinMode(BLpin2, OUTPUT);
+  pinMode(ABCDpin0, OUTPUT);
+  pinMode(ABCDpin1, OUTPUT);
+  pinMode(ABCDpin2, OUTPUT);
+  pinMode(ABCDpin3, OUTPUT);
+  pinMode(LDpin0, OUTPUT);
+  pinMode(LDpin1, OUTPUT);
+  pinMode(LDpin2, OUTPUT);
 }
 
+// sevenSeg_blankPin()
+//     this function sets a specific pin on the seven segment
+//     display to blank out the digit
+void sevenSeg_blankPin( int pin ) {
+  digitalWrite( pin, !PH );
+}
+
+// sevenSeg_blankAll()
+//     this function sets all the pins on the seven segment
+//     display to blank out all the digits
 void sevenSeg_blankAll() {
-  digitalWrite( BLpin0, !PH ); // enable blanking
+  digitalWrite( BLpin0, !PH );
   digitalWrite( BLpin1, !PH );
   digitalWrite( BLpin2, !PH );
 }
 
+// sevenSeg_unblankPin()
+//     this function clears a specific pin on the seven segment
+//     display to unblank the digit
 void sevenSeg_unblankPin( int pin ) {
-  digitalWrite( pin, PH ); // enable blanking
+  digitalWrite( pin, PH );
 }
 
+// sevenSeg_unblankAll()
+//     this function clears all the pins on the seven segment
+//     display to unblank all the digits
 void sevenSeg_unblankAll() {
   digitalWrite( BLpin0, PH ); // enable blanking
   digitalWrite( BLpin1, PH );
   digitalWrite( BLpin2, PH );
 }
 
-
-
-// writes a number to the display. Returns true if that number is 0.
+// sevenSeg_set()
+//     this function writes a number to the seven segment
+//     display, and returns true if the number is 0
 bool sevenSeg_set( int num ) {
 
-  int LDpins[3] = { LDpin0, LDpin1, LDpin2 };
-  int ABCDpins[4] = { ABCDpin0, ABCDpin1, ABCDpin2, ABCDpin3 };
+  int LDpins[3] = {LDpin0, LDpin1, LDpin2};
+  int ABCDpins[4] = {ABCDpin0, ABCDpin1, ABCDpin2, ABCDpin3};
 
-  int digits[3] = { 0, 0, 0 };
-  int ABCD[4] = { 0, 0, 0, 0 };
+  int digits[3] = {0, 0, 0};
+  int ABCD[4] = {0, 0, 0, 0};
 
-//  digitalWrite( BLpin, PH ); // disable blanking
-
-
-  // enable all latches, no changes will be made to 7seg digits (even though ABCD will be changed
-  for( int i = 0; i < numOfDigits; i++ ) {
-    digitalWrite(LDpins[i], LOW );
+  // clear all the latches on the ICs
+  for(int i = 0; i < numOfDigits; i++) {
+    digitalWrite(LDpins[i], LOW);
   }
 
-
-  // Split up num into digits
+  // split the numbers into each digit
   int tempNum = num;
-  for( int i = 0; i < numOfDigits; i++ ) {
-    digits[i] = ( tempNum % (int)pow( 10, i+1 ) ) / (int)pow( 10, i );
-    tempNum = tempNum - digits[i] * (int)pow( 10, i );
+  for(int i = 0; i < numOfDigits; i++) {
+    digits[i] = (tempNum%(int)pow(10, i+1))/(int)pow(10, i);
+    tempNum = tempNum - digits[i]*(int)pow(10, i);
   }
 
-  // blanking
-
-
-  // print digits to 7 seg display
-  for( int i = 0; i < numOfDigits; i++ ) {  // select digit
-
-    // write binary digit to ABCD pins
-    for( int j = 0; j < 4; j++ ) { // read num into ABCD array
-      digitalWrite( ABCDpins[j], bitRead( digits[i], j ) );
+  // print each digit to seven segment display
+  for(int i = 0; i < numOfDigits; i++) {
+    for(int j = 0; j < 4; j++) {
+      digitalWrite(ABCDpins[j], bitRead(digits[i], j));
     }
 
-    // disable latch to write to character
-    digitalWrite( LDpins[i], HIGH );  // disable curr char latch
-    digitalWrite( LDpins[i], LOW ); // all latches enabled
+    // toggle the latches to finish writing the number
+    digitalWrite( LDpins[i], HIGH );
+    digitalWrite( LDpins[i], LOW );
   }
 
+  // unblank all the digits
   sevenSeg_unblankAll();
 
-  if( digits[2] == 0 ) {
-    sevenSeg_blankPin( BLpin2 );
-    if( digits[1] == 0 ) {
-        sevenSeg_blankPin( BLpin1 );
+  // check if there are any leading zeros to blank out
+  if(digits[2] == 0) {
+    sevenSeg_blankPin(BLpin2);
+    if(digits[1] == 0) {
+        sevenSeg_blankPin(BLpin1);
     }
   }
 
-
+  // return true if the number is 0
   if( num == 0 ) {
 		return true;
 	}
+
+  // otherwise return false
 	else {
 		return false;
 	}

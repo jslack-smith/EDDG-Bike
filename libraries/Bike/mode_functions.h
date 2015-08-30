9a2d15f326c26096e7215cd035642b16adae5724
jalinbank <jalinbank@users.noreply.github.com>
2015-08-30 10:42:32 +1000

    Update mode_functions.h
    Clean up, added idleMode, created extern variables

--- libraries/Bike/mode_functions.h
+++ libraries/Bike/mode_functions.h
@@ -1,9 +1,39 @@
+/*  mode_functions.h
+ *   
+ *   This file contains all function prototypes and constant definitions for the different modes.
+ *   
+ *   Change Log: 
+ *      28th August 2015 (JL)
+ *          - clean up
+ *          - added idleMode
+ *          - created extern variables
+ *          
+ *      25th August 2015 (JSS)
+ *          - created file
+ */
+
 #ifndef MODE_FUNCTIONS_H
 #define MODE_FUNCTIONS_H
 
+// include all necessary local header files
 #include "pin_definitions.h"
 #include "LEDring_functions.h"
 
+/***********************************
+ *            Constants            *
+ ***********************************/
+
+
+/***********************************
+ *            Variables            *
+ ***********************************/
+extern int highScore;
+extern bool startBtnPressed;
+
+/***********************************
+ *       Function Prototypes       *
+ ***********************************/
 void startSequence();
+void idleMode();
 
 #endif
